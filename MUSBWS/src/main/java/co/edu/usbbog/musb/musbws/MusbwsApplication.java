56b3bf93b6e6de0670231f18e621bd9a4e82fb8e
NicolasCaro17 <60698612+NicolasCaro17@users.noreply.github.com>
2020-07-23 15:28:33 -0500

    Initial commit

---
+++ MUSBWS/src/main/java/co/edu/usbbog/musb/musbws/MusbwsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.musb.musbws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusbwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusbwsApplication.class, args);
+	}
+
+}
